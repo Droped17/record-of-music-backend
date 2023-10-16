29cfcc819925b07bd29e551f48a1c7f871f5b754
Droped17 <droped17@gmail.com>
2023-10-16 12:49:17 +0700

    add jwt

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
 const authController = require('./controller/auth-controller');
 
 const notfoundMDW = require('./middlewares/not-found');
 const errorMDW = require('./middlewares/error');
 
 app.use(express.json());
+app.use(cors());
+
 
 app.post('/register',authController.register);
 app.post('/login',authController.login);
--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,50 +1,71 @@
 const { PrismaClient } = require("@prisma/client");
 const { registerSchema, loginSchema } = require("../validate/auth-validator");
-const bcrypt = require('bcryptjs');
 const { customError } = require("../utility/custom-error");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res, next) => {
+  console.log(req.body);
   try {
     // ต้องชื่อ value เท่านั้น ไม่งั้นเป็น undefined
-    const { value,error } = registerSchema.validate(req.body); 
+    const { value, error } = registerSchema.validate(req.body);
     // console.log(value);
     if (error) {
-        next(error);
+      next(error);
     }
 
-    const prisma = new PrismaClient();
-    value.password = await bcrypt.hash(value.password,12) //hash password
-    const user = await prisma.user.create({
-      data: value,
-    });
+    // const prisma = new PrismaClient();
+    // value.password = await bcrypt.hash(value.password, 12); //hash password
+    // const user = await prisma.user.create({
+    //   data: value,
+    // });
 
-    res.status(201).json({ message: "register success" ,user});
+    const payload = { userId: user.id };
+    const accessToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY || qwerty17xsdfg22,
+      {
+        expiresIn: process.env.JWT_EXPIRE_DATE
+      }
+    );
+
+    console.log(accessToken);
+    res.status(201).json({ message: "register success" });
   } catch (error) {
     next(error);
   }
 };
 
-exports.login = async (req,res,next) => {
-    try {
-    const {value,error} = loginSchema.validate(req.body);
+exports.login = async (req, res, next) => {
+  try {
+    const { value, error } = loginSchema.validate(req.body);
     if (error) {
-        next(error);
-    }    
+      next(error);
+    }
     // console.log(value);
     const prisma = new PrismaClient();
     // find email
     const user = await prisma.user.findFirst({
-        where: {
-            email: value.email,
-        }
+      where: {
+        email: value.email,
+      },
     });
+
     // not found user
     if (!user) {
-        return next(customError('invalid login'),400);
+      return next(customError("invalid login"), 400);
     }
 
-    res.status(200).json({message: "login success",user});
-    } catch (error) {
-        next(error);
+    // req.password(req) === user.password(db)
+    // warn: ถ้า pass ไม่ได้ bcrypt จะ error
+    const isMatchPass = await bcrypt.compare(value.password, user.password);
+
+    if (!isMatchPass) {
+      return next(customError("invalid login", 400));
     }
-}
+
+    res.status(200).json({ message: "login success", user });
+  } catch (error) {
+    next(error);
+  }
+};
