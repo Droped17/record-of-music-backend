bc60f1cd3674ec49af999495d047487743b7186c
Droped17 <droped17@gmail.com>
2023-10-18 13:51:59 +0700

    create middleware to createProduct

--- routes/authen-route.js
+++ routes/authen-route.js
@@ -3,48 +3,74 @@ const router = express.Router();
 const authController = require("../controller/auth-controller");
 const authenMiddleware = require("../middlewares/authenticate");
 const prisma = require("../models/prisma");
-const uploadMiddleWare  = require("../middlewares/upload");
+const uploadMiddleWare = require("../middlewares/upload");
 
 // Authen
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/me", authenMiddleware, authController.getMe);
 
-
 // Get All Product
 router.get("/product/", async (req, res, next) => {
   const product = await prisma.record.findMany({
-    select:{
-        id: true,
-        albumName: true,
-        price: true,
-        recordInfo: true,
-        image: true,
+    select: {
+      id: true,
+      albumName: true,
+      price: true,
+      recordInfo: true,
+      image: true,
     },
   });
 
-  res.status(201).json({ msg: "get item success" ,product});
+  res.status(201).json({ msg: "get item success", product });
 });
 
 // Get Product By Id
 router.get("/product/:id", async (req, res, next) => {
   console.log(req.params);
-  const {id} = req.params;
+  const { id } = req.params;
   const product = await prisma.record.findFirst({
     where: {
-      id: Number(id)
+      id: Number(id),
     },
   });
   console.log(product);
 
-  res.status(201).json({ msg: "get item success",product});
+  res.status(201).json({ msg: "get item success", product });
 });
+
+
+// Create Product
+router.post("/product/", async (req, res, next) => {
+  try {
+    const { albumName, genreName, price, image, recordInfo, score, artistId } =
+      req.body;
+    const newProduct = await prisma.record.create({
+      data: {
+        albumName: albumName,
+        genreName: genreName,
+        price: price,
+        image: image,
+        recordInfo: recordInfo,
+        score: score,
+        artistId: artistId,
+      },
+    });
+
+    console.log(newProduct);
+
+    res.status(200).json({ msg: "add product success", newProduct });
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 
 
 //Upload Image
 // router.post("/upload",authenMiddleware,uploadMiddleWare.single("auto"),(req,res,next)=>{
-  
+
 // });
 
 module.exports = router;
